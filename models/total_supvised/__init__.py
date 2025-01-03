6c4ede11c5faafc0aaa4999a862214afaed2e394
Michaelzyb <1287293308@qq.com>
2025-01-03 21:56:13 +0800

    __init__.py

--- models/total_supvised/__init__.py
+++ models/total_supvised/__init__.py
@@ -2,7 +2,7 @@ from .edrnet import EDRNet
 from .bisenet import BiSeNet
 from .deeplabv3 import DeepLabV3
 from .seg_net import SegNet
-from .swin_unet import SwinTransformerSys
+from .abunet import ABUNet
 from .seg_former import segformer_b0
 from .pga_net import PGANet
 from .topformer import TopFormer
